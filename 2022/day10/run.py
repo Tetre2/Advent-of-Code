820028cada6b0b923a832e5202499bc1e27c35d6
Tetre2 <mattias.retteli@gmail.com>
2022-12-10 16:45:47 +0100

    day10 cleanup

--- 2022/day10/run.py
+++ 2022/day10/run.py
@@ -1,71 +1,37 @@
 f = open('input.txt', 'r')
 data = f.readlines()
 
+instructiuon = None
 cycle = 0
 signal_sum = 0
 reg_x = 1
 # Part 1
-# def check_signal_strength():
-#     global signal_sum
-#     if (cycle - 20) % 40 == 0:
-#         signal_sum += (reg_x * cycle)
-
-# for instructiuon in data:
-#     cycle += 1
-#     check_signal_strength()
-
-#     if instructiuon[0] == 'a':
-#         cycle += 1
-#         check_signal_strength()
-#         val = int(instructiuon.split(" ")[1])
-#         reg_x += val
-
-# print(signal_sum)
-    
+def check_signal_strength():
+    global signal_sum
+    if (cycle - 20) % 40 == 0:
+        signal_sum += (reg_x * cycle)
 
 # Part 2
-instructiuon = None
-screen = []
-for _ in range(6):
-    screen.append(list("." * 40))
 row = ""
-y = 0
 pixel = 0
-
 def paint():
     global row
     global pixel
-    global y
-    
-    print(cycle, pixel, reg_x)
-    
+
     if pixel == reg_x - 1 or pixel == reg_x or pixel == reg_x + 1:
         row = row + "#"
-        screen[y][pixel] = "#"
     else:
         row = row + "."
-        screen[y][pixel] = "."
-    
-    print("".join(screen[y]))
-    s = ""
-    for i in range(40):
-        if i == reg_x - 1 or i == reg_x or i == reg_x + 1:
-            s = s + "$"
-        else:
-            s = s + "-"
-    print(s)
-    
-    pixel += 1
 
+    pixel += 1
     if pixel % 40 == 0:
         print(row)
         row = ""
-        y += 1
         pixel = 0
-
 
 while cycle < 240:
     cycle += 1
+    #check_signal_strength()
     paint()
 
     if instructiuon != None:
@@ -78,8 +44,4 @@ while cycle < 240:
             instructiuon = None
 
 
-for i in screen:
-    print("".join(i))
-
-
-
+#print(signal_sum)
