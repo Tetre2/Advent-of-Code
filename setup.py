7144a3af963cf148cf4ffcf7a3c98d2afbdcaa51
Tetre2 <mattias.retteli@gmail.com>
2023-12-01 20:17:05 +0100

    added test file creation

--- setup.py
+++ setup.py
@@ -15,14 +15,20 @@ except:
     print("Directory already exists for today")
     exit()
     
-open(f"{path}/run.py", "x").write("f = open('input.txt', 'r')\ndata = f.readlines()\n\n\n\n")
+open(f"{path}/run1star.py", "x").write("f = open('test1star.txt', 'r')\n\ndata = f.readlines()\nt = open('test1star.txt', 'r')\ntestdata = t.readlines()\n\n\nresults = ''\nprint(results)\n")
+
+open(f"{path}/run2star.py", "x").write("f = open('test2star.txt', 'r')\n\ndata = f.readlines()\nt = open('test2star.txt', 'r')\ntestdata = t.readlines()\n\n\nresults = ''\nprint(results)\n")
+
+open(f"{path}/test1star.txt", "x").write("")
+
+open(f"{path}/test2star.txt", "x").write("")
 
 cookie = open("./.session", "r").readline().splitlines()[0]
 
 s = requests.session()
 s.cookies.set("session", cookie, domain=".adventofcode.com")
-respons = s.get(f"https://adventofcode.com/{year}/day/{day}/input")
+response = s.get(f"https://adventofcode.com/{year}/day/{day}/input")
 
-open(f"{path}/input.txt", "x").write(respons.text)
+open(f"{path}/input.txt", "x").write(response.text)
 
 print("Creating directory for day" + str(day))
