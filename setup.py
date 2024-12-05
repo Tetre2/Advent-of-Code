8b8728856af7ee52ee4703c113d9f52a35ec5fc0
Tetre2 <mattias.retteli@gmail.com>
2024-12-05 18:38:28 +0100

    added args to setup

--- setup.py
+++ setup.py
@@ -1,32 +1,38 @@
 import os
 from datetime import date
-from requests import get
 import requests
 import shutil
+import argparse
 
+parser = argparse.ArgumentParser("Setup.py")
+parser.add_argument('-f', '--force', action='store_true')
+parser.add_argument('-d', '--day')
+args = parser.parse_args()
+print(args.force, args.day)
 
 year, _, day = str(date.today()).split("-")
-day = day.lstrip('0')
+day = args.day if args.day else day.lstrip('0')
 path = f"./{year}/day{day}"
 
 try:
     os.makedirs(path)
 except:
-    print("Directory already exists for today")
-    exit()
+    print(f"Setup already exists for day {day} {year}")
+    if not args.force:
+        exit()
 
 shutil.copyfile('template.py', f"{path}/run.py")
 
-open(f"{path}/part1Sample.txt", "x").write("")
-
-open(f"{path}/part2Sample.txt", "x").write("")
-
+session = requests.session()
 cookie = open("./.session", "r").readline().splitlines()[0]
+session.cookies.set("session", cookie, domain=".adventofcode.com")
+response = session.get(f"https://adventofcode.com/{year}/day/{day}/input")
 
-s = requests.session()
-s.cookies.set("session", cookie, domain=".adventofcode.com")
-response = s.get(f"https://adventofcode.com/{year}/day/{day}/input")
+failIfExists = "x"
+if args.force: failIfExists = "w"
 
-open(f"{path}/input.txt", "x").write(response.text)
+open(f"{path}/input.txt", failIfExists).write(response.text)
+open(f"{path}/part1Sample.txt", failIfExists).write("")
+open(f"{path}/part2Sample.txt", failIfExists).write("")
 
-print("Creating directory for day" + str(day))
+print(f"Creating setup for day {day}")
